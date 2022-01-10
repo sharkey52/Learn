53c978536c3d07fbf45ad6eac16bf6bc16462fd5
Eserbailey <97388012+Eserbailey@users.noreply.github.com>
2022-01-10 21:18:02 +0000

    Add files via upload

---
+++ Countdown to Prague.py
@@ -0,0 +1,12 @@
+import datetime
+
+print ('*Countdown to Prague*')
+
+prague = datetime.date(2022, 2, 11) - datetime.date.today()
+
+prague = str(prague).split(',')
+
+print ('We have ' + str(prague[0]) + ' left till we fly to Prague!!!')
+
+
+
